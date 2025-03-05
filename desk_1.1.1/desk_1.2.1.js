bddc5c6371d3e73a59d58094666c621457733fe6
Софія Півторак <sofiapivtorak@MacBook-Pro-Sofia.local>
2025-03-05 01:02:52 -0800

    Initial commit

---
+++ desk_1.1.1/desk_1.2.1.js
@@ -0,0 +1,13 @@
+let userNames = [`Sofiia`];
+let userNameCorrect;
+do {
+    userName = prompt("Enter your name");
+    console.log(`User name: ${userName}`);
+
+    isNameCorrect = confirm(`Is your name ${userName}?`);
+    console.log(`User name correct: ${isNameCorrect}`);
+}
+while (!isNameCorrect);
+
+userNames.push(userName);
+console.log(userNames);
