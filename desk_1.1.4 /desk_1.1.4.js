9b54f4998180f7e7b99f725456c30e33832f3688
aaandriivna <sophiapivtorak@gmail.com>
2025-03-05 14:08:37 -0800

    користувач вводить бажану довжину пароля

--- desk_1.1.4 /desk_1.1.4.js
+++ desk_1.1.4 /desk_1.1.4.js
@@ -1,1 +1,24 @@
+// Запитуємо у користувача довжину пароля
+let length = prompt("Введіть довжину пароля:");
+
+// Перетворюємо введене значення у число
+length = parseInt(length);
+
+// Перевіряємо, чи введено коректне число
+if (isNaN(length) || length <= 0) {
+    alert("Будь ласка, введіть коректну довжину (позитивне число)!");
+} else {
+    // Символи, з яких будемо генерувати пароль
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let password = "";
+
+    // Генеруємо пароль випадковим чином
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    // Виводимо згенерований пароль
+    alert("Ваш випадковий пароль: " + password);
+}
 
